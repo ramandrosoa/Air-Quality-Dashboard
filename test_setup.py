991bdc5cb0544d0c9fd62ec0a3290efe33f5345f
ramandrosoa <mramandrosoa@hawk.iit.edu>
2026-03-10 02:19:01 +0300

    Delete test_setup.py

--- test_setup.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import requests 
-import folium
-
-print("All packages imported successfully")
-print(f"Pandas version: {pd.__version__}")
-print(f"Requests version: {requests.__version__}")
